f5aa9fcfde3fc3899bed7784eb036f8e15539a89
김진해 <pj.cabman21@kalmate.net>
2024-08-19 17:08:09 +0900

    multi call

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,8 +1,19 @@
+import Link from "next/link";
+
 const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovie(id: string) {
-  const reponse = await fetch(`${API_URL}/${id}`);
-  return reponse.json();
+  console.log(`Fetching movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
 }
 
 export default async function MovieDetail({
@@ -10,8 +21,18 @@ export default async function MovieDetail({
 }: {
   params: { id: string };
 }) {
-  //   console.log(props);
-  console.log("id: %s", id);
+  console.log("start fetching");
   const movie = await getMovie(id);
-  return <h1>{movie.title}</h1>;
+  const videos = await getVideos(id);
+  console.log("end fetching");
+  return (
+    <div>
+      <h1>{movie.title}</h1>
+      {videos.map((video) => (
+        <li key={video.id}>
+          <Link href={`/movies/${video.id}`}>{video.name}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
