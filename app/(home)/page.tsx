6fc3992ba171ea25363444b3e9baad586249a089
김진해 <pj.cabman21@kalmate.net>
2024-08-19 15:23:26 +0900

    useState, useEffect

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,20 @@
-export const metadata = {
-  title: 'Home',
-  description: 'The best Movies on the best framework',
-}
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Root() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
 }
