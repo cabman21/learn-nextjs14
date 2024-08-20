9227c325a9eaedd9e98419989e82cc5c55d058d1
김진해 <pj.cabman21@kalmate.net>
2024-08-20 13:51:42 +0900

    add videos

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,3 +1,4 @@
+import tomato from "../styles/movie-info.module.css";
 import { API_URL } from "../app/(home)/page";
 
 async function getMovie(id: string) {
@@ -9,5 +10,19 @@ async function getMovie(id: string) {
 
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={tomato.container}>
+      <img
+        src={movie.poster_path}
+        alt={movie.title}
+        className={tomato.poster}
+      />
+      <div className={tomato.info}>
+        <h1 className={tomato.title}>{movie.title}</h1>
+        <h3>⭐️ {movie.vote_average.toFixed(2)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={"_blank"}>Homepage &rarr;</a>
+      </div>
+    </div>
+  );
 }
