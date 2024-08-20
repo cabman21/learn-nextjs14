0975bc9e2f5a5ef911bf616379cd20568884d870
김진해 <pj.cabman21@kalmate.net>
2024-08-20 12:02:31 +0900

    add css

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,23 +2,24 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
   const path = usePathname();
   console.log(path);
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
-          <Link href="/">Home</Link> {path === "/" ? "*" : ""}
+          <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
         </li>
         <li>
           <Link href="/about-us">About Us</Link>{" "}
-          {path === "/about-us" ? "*" : ""}
+          {path === "/about-us" ? "🔥" : ""}
         </li>
         <li>
           <Link href="/about-us/company/jobs/sales">Sales</Link>{" "}
-          {path === "/about-us/company/jobs/sales" ? "*" : ""}
+          {path === "/about-us/company/jobs/sales" ? "🔥" : ""}
         </li>
         <li>
           <Link href="/movies/121212?region=kr&page=2">Movie</Link>
